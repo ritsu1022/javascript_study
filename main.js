7e94127f5e26eb8bd72b55aa0923b352d6f8ac86
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-19 22:26:56 +0900

    テンプレートリテラルの使用

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const a = 1;
-const b = '2';
-const answer = a + b;
-console.log(answer);
+const lastName = '右寺';
+const firstName = '隆信';
+const birthday = '1982';
+const hometown = '佐賀';
+
+const greeting = `私の名前は${lastName}${firstName}です。${birthday}年生まれで、${hometown}出身です。`;
+console.log(greeting);
