f815b4f487daddd0a0021ccaa62f05fa677e62b7
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-20 23:00:00 +0900

    配列、値の書き換え

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-const numbers = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8, 9]
-];
+const animals = ['cat', 'dog', 'bird'];
+console.log(animals);
 
-console.log(numbers[1][2]);
+animals[0] = 'human';
+console.log(animals);
