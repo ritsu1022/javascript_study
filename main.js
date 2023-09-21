75384164b1a2cb6a84bdfe2df492e058503e099d
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-21 22:09:24 +0900

    アロー関数

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const area1 = getTriangleArea(4, 5);
-console.log(area1);
-
-const getTriangleArea = function (base, height) {
+const getTriangleArea = (base, height) => {
   const area = (base * height) / 2;
   return area;
 };
+
+const area1 = getTriangleArea(4, 5);
+console.log(area1);
