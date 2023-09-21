d9fb64ba995586d2f83788c218db35e5fcf5e6ff
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-21 22:04:21 +0900

    巻き上げない例

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const area1 = getTriangleArea(4, 5);
 console.log(area1);
 
-function getTriangleArea(base, height) {
+const getTriangleArea = function (base, height) {
   const area = (base * height) / 2;
   return area;
-}
+};
