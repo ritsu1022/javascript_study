90e698bc7675ed7e04bccff78ee1915dc00ae8e6
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-22 22:29:30 +0900

    スコープチェーン挙動確認

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const showLog = () => {
-  const a = 1;
+const a = 'グローバルスコープにおいてのa';
+
+const func1 = () => {
+  const a = 'func1関数においてのa';
   console.log(a);
+
+  const func2 = () => {
+    const a = 'func2関数においてのa';
+    console.log(a);
+  };
+  func2();
 };
 
-showLog();
 console.log(a);
+func1();
