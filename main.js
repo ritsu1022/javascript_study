e1f18c70a4390f4e84b641294041e3c4489396df
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-23 19:20:03 +0900

    論理演算子　否定

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 //数学と英語の点数が、どちらかが80点以上であれば合格の場合（OR条件）
-const getIsSuccess = (math, english) => {
-  if (math >= 80 || english >= 80) {
-    return '合格';
-  } else {
-    return '不合格';
-  }
-};
+const bool1 = true;
+console.log(!bool1);
 
-console.log(getIsSuccess(60, 90));
-console.log(getIsSuccess(90, 90));
-console.log(getIsSuccess(60, 70));
+const bool2 = false;
+console.log(!bool2);
