e6988329a37055cfd2a437aaaed7511ba1e05133
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-23 19:02:34 +0900

    比較演算子

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-const checkBMI= (height, weight) => {
-  const bmi = weight / (height * height);
-  if (bmi > 25) {
-    if (bmi >= 40) {
-      return '太りすぎです、肥満度は4度です';
-    } else if (bmi > 35) {
-      return '太りすぎです、肥満度は3度です';
-    } else if (bmi > 30) {
-      return '太りすぎです、肥満度は2度です';
-    } else {
-      return '太りすぎです、肥満度は1度です';
-    }
-  } else if (bmi < 18.5) {
-    return '痩せすぎです';
-  } else {
-    return '標準体型です';
-  }
-};
+const a = 3;
+const b = 5;
 
-console.log(checkBMI(1.5, 40));
-console.log(checkBMI(1.5, 108));
+console.log(a <= b);
+console.log(a < b);
+console.log(a > b);
+console.log(a >= b);
