1a03ff3116d34ebe893890f6c4bf38d92059653f
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-23 19:17:31 +0900

    論理演算子　OR条件

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-//数学と英語の点数が、両方ともに80点以上であれば合格の場合（AND条件）
+//数学と英語の点数が、どちらかが80点以上であれば合格の場合（OR条件）
 const getIsSuccess = (math, english) => {
-  if (math >= 80 && english >= 80) {
+  if (math >= 80 || english >= 80) {
     return '合格';
   } else {
     return '不合格';
