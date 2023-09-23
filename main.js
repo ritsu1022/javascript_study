b372f57c87adb61787e4421f43c45341d75da240
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-23 19:07:56 +0900

    論理演算子

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-console.log(5 === '5');
-console.log(5 == '5');
+//数学と英語の点数が、両方ともに80点以上であれば合格の場合（AND条件）
+const getIsSuccess = (math, english) => {
+  if (math >= 80 && english >= 80) {
+    return '合格';
+  } else {
+    return '不合格';
+  }
+};
+
+console.log(getIsSuccess(60, 90));
+console.log(getIsSuccess(90, 90));
+console.log(getIsSuccess(60, 70));
