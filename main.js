8fcb3f32fd2079127bf0c42e0c6e7679f7f29deb
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-25 21:02:38 +0900

    クセのある真偽値判定1

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-const cry = animal => {
-  switch (animal) {
-    case 'cat':
-      console.log('にゃー');
-      break;
-    case 'dog':
-      console.log('わんわん');
-      break;
-    case 'bird':
-      console.log('ちゅんちゅん');
-      break;
-    case 'lion':
-      console.log('がおー');
-      break;
-    case 'elephant':
-      console.log('ぱおーん');
-      break;
-    default:
-      console.log('......');
-  }
-};
-
-cry('dog');
+const value = 0;
+if (value) {
+  console.log(true);
+} else {
+  console.log(false);
+}
