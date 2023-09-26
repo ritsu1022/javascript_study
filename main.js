850cc8050b17c1278419f79c7b10e36b3c80e493
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-26 20:13:30 +0900

    FizzBuzz問題

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-let result = 0;
-for(let i = 1; i <= 10; i++) {
-  result += i;
+for( let i = 1; i <= 100; i++) {
+  if(i % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
 }
-
-console.log(result);
