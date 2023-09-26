93f575ced1110549b0f324a09c4bc278a933b729
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-26 20:15:18 +0900

    配列の繰り返し文

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-for( let i = 1; i <= 100; i++) {
-  if(i % 15 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
-  }
+const animals = ['cat', 'dog', 'bird'];
+
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
 }
