59b861e0455b1f4fcf9801b2230903a8804c816c
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-26 20:46:03 +0900

    filter

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const numbers1 = [1, 3, 8];
 const numbers2 = [7, 9, 13];
 
-console.log(numbers1.some(number => number > 10));
-console.log(numbers2.some(number => number > 10));
+console.log(numbers1.filter(number => number > 10));
+console.log(numbers2.filter(number => number > 10));
