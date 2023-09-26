37a63f48e423ef7eca73504a6cd6639214e75459
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-26 20:42:07 +0900

    someを使わない例文

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const numbers = [1, 3, 8];
-const result = numbers.map(number => number * 2);
+const numbers1 = [1, 3, 8];
+const numbers2 = [7, 9, 13];
 
-console.log(result);
+let result1 = 1;
+
+numbers1.forEach(number => {
+  result1 = result1 * number > 10 ? 0 : 1;
+});
+console.log(result1 === 0 ? true : false);
+
+let result2 = 1;
+
+numbers2.forEach(number =>  {
+  result2 = result2 * number > 10 ? 0 : 1;
+});
+console.log(result2 === 0 ? true : false);
