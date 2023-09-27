afed3a6c810699513b519b303f7c5ed76f38ab5e
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-28 00:36:50 +0900

    thisの関数呼び出し

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-const myProfile = {
-  lastName: '右寺',
-  firstName: '隆信',
-  birthyear: '1982',
-  hometown: '佐賀',
-  sayHello() {
-    console.log('Hello');
-  },
-  greet() {
-    console.log(
-      `私の名前は${this.lastName}${this.firstName}です。${this.birthyear}年生まれで、${this.hometown}出身です。`
-    );
-  },
-};
+function func() {
+  console.log(this);
+}
 
-myProfile.greet();
+func();
