6fb91c864cff42268dd5e3ef48d4825ee9719337
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-28 00:14:24 +0900

    Mathオブジェクトの例

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-const str = 'abc';
+// 値を四捨五入するためのメソッド
+console.log(Math.round(4.6));
 
-console.log(str.length);
-console.log(str.toUpperCase());
+// 絶対値を取得するメソッド
+console.log(Math.abs(-3));
+
+// 配列内の最大値を取得するメソッド
+console.log(Math.max(1, 3, 5));
+
+// 配列内の最小値を取得するメソッド
+console.log(Math.min(1, 3, 5));
+
+// ネイピア数を表すプロパティ
+console.log(Math.E);
+
+// 自然対数を取得するメソッド
+console.log(Math.log(Math.E));
+
+// 0以上1未満のランダムな数を取得するメソッド
+console.log(Math.random());
+
+// 累乗した値を取得するメソッド
+console.log(Math.pow(10, 2));
+
+// ただし、累乗はES2016以降では下記の演算子も利用可能
+console.log(10 ** 2);
