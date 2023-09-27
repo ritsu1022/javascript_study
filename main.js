7e4ce550e4c27e31ac091b979d6f7dedb3031f70
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-27 23:57:23 +0900

    ビルドインオブジェクトの確認

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const myProfile = {
-  lastName: '右寺',
-  firstName: '隆信',
-  birthyear: '1982',
-  hometown: '佐賀',
-  sayHello() {
-    console.log('Hello');
-  }
-};
+const animals = ['cat', 'dog', 'bird']
 
-myProfile.sayHello();
+console.log(animals);
