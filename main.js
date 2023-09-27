dffe6807211694b59e890e6db41747327c5f93de
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-28 00:21:35 +0900

    Math.random()の使い方

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-// 値を四捨五入するためのメソッド
-console.log(Math.round(4.6));
+const getRandomNumber = (start, end) => Math.floor(start + Math.random() * (end + 1 - start));
 
-// 絶対値を取得するメソッド
-console.log(Math.abs(-3));
-
-// 配列内の最大値を取得するメソッド
-console.log(Math.max(1, 3, 5));
-
-// 配列内の最小値を取得するメソッド
-console.log(Math.min(1, 3, 5));
-
-// ネイピア数を表すプロパティ
-console.log(Math.E);
-
-// 自然対数を取得するメソッド
-console.log(Math.log(Math.E));
-
-// 0以上1未満のランダムな数を取得するメソッド
-console.log(Math.random());
-
-// 累乗した値を取得するメソッド
-console.log(Math.pow(10, 2));
-
-// ただし、累乗はES2016以降では下記の演算子も利用可能
-console.log(10 ** 2);
+console.log(getRandomNumber(50, 100));
