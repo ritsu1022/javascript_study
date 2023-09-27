51009e85c3e49699656eb0019fa31ac658f885c5
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-28 00:42:15 +0900

    thisの使用法の違いによるエラー

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function func() {
-  console.log(this);
+const obj = {
+  str: 'abc',
+  func1() {
+    console.log(this.str); // メソッド呼び出し
+    
+    const func2 = function() {
+      console.log(this.str); // 関数呼び出し
+    };
+    func2();
+  }
 }
 
-func();
+obj.func1();
