2e7ca01d1d2ce5b0c6771ab43748508414c5da9c
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-29 21:52:05 +0900

    要素の削除

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 const target = document.getElementById('main').firstElementChild;
 
-target.style.fontSize = '.5em';
-target.style.border = '1px solid #000';
-target.style.marginBottom = '100px';
+target.parentNode.removeChild(target);
