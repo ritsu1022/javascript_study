a75eff9360a31318f2470a781bd20289538d5a0b
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-29 21:13:22 +0900

    id名で要素を取得する

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-class Person {
-  constructor(name, born) {
-    this.name = name;
-    this.born = born;
-  }
-  greet() {
-    console.log(`Hello, my name is ${this.name}.`);
-  }
-}
+const target = document.getElementById('main');
 
-class Musician extends Person {
-  constructor(name, born, part) {
-    super(name, born);
-    this.part = part;
-  }
-  introduce() {
-    console.log(`My part is ${this.part}.`);
-  }
-}
-
-const john = new Musician('John Lennon', '1940', 'rhythm guitar');
-
-console.log(john.name);
-john.greet();
-console.log(john.part);
-john.introduce();
+console.log(target);
