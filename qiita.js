3c9761d6952ad7d086310702b302688d37b23411
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-30 23:31:53 +0900

    asyncを使用した書き換え

--- qiita.js
+++ qiita.js
@@ -1,18 +1,14 @@
 const btn = document.getElementById('btn');
 const list = document.getElementById('list');
 
-btn.addEventListener('click', () => {
-  fetch('https://qiita.com/api/v2/items?query=tag:JavaScript')
-  .then(response => {
-    console.log(response);
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    data.forEach(item => {
-      const li = document.createElement('li');
-      li.innerHTML = `<a href="${item.url}" target="_blank">${item.title}</a>`;
-      list.appendChild(li);
-    });
+btn.addEventListener('click', async () => {
+  const response = await fetch(
+    'https://qiita.com/api/v2/items?query=tag:JavaScript'
+  );
+  const data = await response.json();
+  data.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="${item.url}" target="_blank">${item.title}</a>`;
+    list.appendChild(li);
   });
 });
