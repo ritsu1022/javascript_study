0368333b56b72a561e00298a2d8bbf4edb938c33
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-30 22:34:20 +0900

    親要素へのイベント伝播停止

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const target1 = document.getElementById('main');
 const target2 = target1.firstElementChild;
 
 target1.addEventListener('click', () => {
-  console.log('target1をクリックしました')
+  console.log('target1をクリックしました');
 });
 
-target2.addEventListener('click', () => {
-  console.log('target2をクリックしました')
+target2.addEventListener('click', e => {
+  console.log('target2をクリックしました');
+  e.stopPropagation();
 });
