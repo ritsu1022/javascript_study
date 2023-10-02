0f30c96305ce5e2ebeb198f174407470a53a7844
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-02 23:05:33 +0900

    16進数と8進数

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let c = 1;
-let d = 1.23;
+let e = 0xff;
+let f = 077;
 
-console.log(typeof(c));
-console.log(typeof(d));
+console.log(e);
+console.log(typeof(e));
+console.log(f);
+console.log(typeof(f));
