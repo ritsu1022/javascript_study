61100ff848309397ec6a443907469db99f7fa95f
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-06 23:46:03 +0900

    0の＋ー

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-console.log(null - 0);
-console.log(true - 0);
-console.log(false - 0);
-console.log(123 - 0);
-console.log("aaa" - 0);
-console.log("123" - 0);
-console.log("0xffff" - 0);
+console.log(1 / +0);
+console.log(1 / 0);
+console.log(1 / -0);
