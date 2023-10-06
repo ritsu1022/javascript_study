81d8bb7252c2a43a0325d4b253a3830d0a0359f7
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-06 23:50:49 +0900

    NaNの例

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-console.log(1 / +0);
-console.log(1 / 0);
-console.log(1 / -0);
+console.log(0 / 0);
+console.log(Number("1"));
+console.log(Number("A"));
+console.log(Number("A") == Number("B"));
+console.log(isNaN(Number("A")));
