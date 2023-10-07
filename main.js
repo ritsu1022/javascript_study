06dc3bde9e0b3778d6e4c70e835a98204aa9028f
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-07 21:50:16 +0900

    forブロック内のローカルスコープ

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 let a = "global value"; // グローバル変数宣言
 
 function f1() {
-  let a = "local value"; // ブロック内のローカル変数宣言
-  return a;
+  let a = "local value in function"; // ブロック内のローカル変数宣言
+  console.log(a);
+
+  for (var i = 0; i < 2; i++) {
+    let a = "value in for loop"; // ブロック内のローカル変数宣言
+    console.log(a);
+  }
 }
 
-function f2() {
-  a = "value changed"; // letやvar、const省略のグローバル変数
-}
-
-console.log(a);
-console.log(f1());
-console.log(a);
-f2();
-console.log(a);
+f1();
