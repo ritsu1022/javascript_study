04b4dbf7e45c6083acec57119fcaee3095608f6d
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-07 21:43:53 +0900

    変数のスコープ

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-console.log(isNaN("foo"));
-console.log(Number("A") !== Number("A"));
-console.log("foo" !== "foo");
+let a = "global value"; // グローバル変数宣言
+
+function f1() {
+  let a = "local value"; // ブロック内のローカル変数宣言
+  return a;
+}
+
+function f2() {
+  a = "value changed"; // letやvar、const省略のグローバル変数
+}
+
+console.log(a);
+console.log(f1());
+console.log(a);
+f2();
+console.log(a);
