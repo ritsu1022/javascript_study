51e7719888c8afd2a96f5e1f627889cb0bb7e359
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-07 22:39:19 +0900

    Infinityの例

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-let a = "global value"; // グローバル変数宣言
-
-function f1() {
-  let a = "local value in function"; // ブロック内のローカル変数宣言
-  console.log(a);
-
-  for (var i = 0; i < 2; i++) {
-    let a = "value in for loop"; // ブロック内のローカル変数宣言
-    console.log(a);
-  }
-}
-
-f1();
+console.log(1 / 0);
+console.log(-1 / 0);
+console.log((-1 / 0) == (-2 / 0));
