17fdd34ca10cdcc368df80b24a04fce476b8f271
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-09 21:43:21 +0900

    文字列結合演算子

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-console.log(0xff & 0x0f); // AND
-console.log(0xf0 | 0x0f); // OR
-console.log(0xf0 ^ 0x0f); // XOR
-console.log(~0x0000ffff); // NOT
-console.log(0x08 << 2); // 左シフト
-console.log(0x08 >> 2); // 右シフト
-console.log(-15 >>> 2); // 符号なし右シフト
-console.log(-15 >> 2); // 右シフト
+console.log("This is " + "Japan" + ".");
