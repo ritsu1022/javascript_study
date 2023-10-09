3596648316a44788040afa2b22bcd48ddbb11f56
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-09 21:15:37 +0900

    算術演算子（2項）

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-console.log(-1);
-
-let apple = "10";
-console.log(apple);
-console.log(+apple);
-
-let ab = 10;
-console.log(++ab);
-console.log(ab);
-
-let ac = 10;
-console.log(ac++);
-console.log(ac);
-
-let ad = 10;
-console.log(--ad);
-console.log(ad);
-
-let ae = 10;
-console.log(ae--);
-console.log(ae);
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+console.log(6 % 3);
