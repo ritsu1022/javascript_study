d9a8056146d2c5eecf5a3581e146be75b3740ec0
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-09 21:13:44 +0900

    算術演算子（単項）

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log(Number.MIN_VALUE);
+console.log(-1);
+
+let apple = "10";
+console.log(apple);
+console.log(+apple);
+
+let ab = 10;
+console.log(++ab);
+console.log(ab);
+
+let ac = 10;
+console.log(ac++);
+console.log(ac);
+
+let ad = 10;
+console.log(--ad);
+console.log(ad);
+
+let ae = 10;
+console.log(ae--);
+console.log(ae);
