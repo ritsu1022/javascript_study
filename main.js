953974306c6a9e921e479efcfafd236894928a25
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-10 21:19:27 +0900

    ?:条件演算子

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 let x = 2;
 let y = 3;
 
-x == y? console.log("x = y"):console.log("x <> y")
-
-let a = 3;
-let b = 3;
-
-a == b? console.log("a == b"):console.log("a <> b")
+x == y? console.log("x = y"):console.log("x <> y");
