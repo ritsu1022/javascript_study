b7eca841b33836f9ed1ca54ffe8358a288961819
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-10 21:38:03 +0900

    continue

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-let n = 0;
-while (1) {
-  if ( n > 100 )
-    break;
-  n++;
+let c = 0;
+
+// 1~12月分のﾙｰﾌﾟ処理
+for ( let month = 1; month <= 12; month++ ) {
+
+  // 6~8月は加算処理をｽｷｯﾌﾟ
+  if ( month >= 6 && month <= 8) continue;
+
+  c++;
 }
 
-console.log(n);
+console.log(c);
