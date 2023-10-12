c435a902852b9ad915373d0a27c77af56315396d
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-12 23:34:47 +0900

    型が異なるオブジェクトの代入

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-let birthDay = [["foo", "8/4"],
-                ["bar", "12/24"],
-                ["hoge", "2/14"]];
+let d = [1, "a", false];
 
-console.log(birthDay[0][0]);
-console.log(birthDay[0][1]);
-console.log(birthDay[1][0]);
-console.log(birthDay[1][1]);
-console.log(birthDay[2][0]);
-console.log(birthDay[2][1]);
+console.log(d[0]);
+console.log(d[1]);
+console.log(d[2]);
