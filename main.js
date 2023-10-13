ce5c5647e1d311466ac95d3de2cc5d58c7119886
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-13 23:02:00 +0900

    プロトタイプベースの継承例

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-let mike = {name:"Mike"};
+function Person(name) {
+  this.name = name;
+}
 
-console.log(mike.propertyIsEnumerable('name'));
+// コンストラクタを呼び出し、オブジェクトnewPersonを作成
+let newPerson = new Person("Mike");
+
+// コンストラクタのprototypeプロパティにプロパティsexを追加
+Person.prototype.sex = "male";
+
+// オブジェクトnewPersonにsexプロパティが追加されている
+console.log(newPerson.sex);
