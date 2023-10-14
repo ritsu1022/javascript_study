772eb05a749ab56d744b4a14701bc06372636f08
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-14 22:01:47 +0900

    function文での残余引数の例

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-let Human = function(name, age) {
-  this.name = name;
-  this.age = age;
-};
-
-let mike = new Human("Mike", 16);
-
-// 関数オブジェクトにsleepメソッドを追加
-mike.sleep = () => {
-  return ("Good night!");
-};
-
-console.log(mike.sleep());
+function number(...args) {
+  console.log(args);
+}
+number(1, 2, 3, 4, 5);
