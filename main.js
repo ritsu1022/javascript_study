ab014af484a6ace660253c9e81f85a9401b831f5
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-15 23:10:07 +0900

    スコープチェーンの例

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-let num = [1, 2, 3, 4, 5];
+const a = "global";
 
-console.log(...num);
+console.log(a);
+
+f1();
+
+console.log(a);
+console.log(z);
+
+function f1() {
+  let a = "local-f1";
+  let z = "local-f1";
+
+  console.log(a);
+  console.log(z);
+
+  f2();
+
+  function f2() {
+    let a = "local-f2";
+
+    console.log(a);
+    console.log(z);
+  }
+}
