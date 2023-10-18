8f253b1c15627763466cf639ebf1f3683e07371f
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-18 23:53:58 +0900

    isFinite関数の呼び出し

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-let str = "This is Japan.";
-let regexp1 = new RegExp("japan", "i");
-
-let regexp2 = new RegExp("japan");
-console.log(str.match(regexp1));
-console.log(str.match(regexp2));
+console.log(isFinite(1/0));
