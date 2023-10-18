05a80fce73c3ebfa72ec00066118da2aae9dce0c
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-18 23:52:24 +0900

    RegExpオブジェクトの例

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-try {
-  throw new Error("Original Error");
-} catch (e) {
-  console.log(e.name + "." + e.message);
-}
+let str = "This is Japan.";
+let regexp1 = new RegExp("japan", "i");
+
+let regexp2 = new RegExp("japan");
+console.log(str.match(regexp1));
+console.log(str.match(regexp2));
