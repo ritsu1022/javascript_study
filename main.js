05e35a925f9c473f6b1c7ea092b590207ebd4d3f
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-18 23:46:41 +0900

    Errorオブジェクトの利用例

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-let member = {
-  foo:{
-    number : 1,
-    name   :"foo",
-  },
-  bar:{
-    count  : 2,
-    name   :"bar",
-  },
-  hoge:{
-    count  : 3,
-    name   :"hoge",
-  }
-};
-
-let json_str = JSON.stringify(member);
-
-console.log(json_str);
+try {
+  throw new Error("Original Error");
+} catch (e) {
+  console.log(e.name + "." + e.message);
+}
