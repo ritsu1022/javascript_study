e75cf39d09474c0b5bbd41a69d5df688e5e8fd53
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-19 23:32:50 +0900

    URIのエンコードとデコード

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log(isFinite(1/0));
+let url = "http://foo.bar?str=";
+let search = "字1+字2";
+
+let uri1 = url + search;
+
+let uriEncoded1 = encodeURI(uri1);
+
+let uriEncoded2 = encodeURIComponent(search);
+
+console.log(uri1);
+
+console.log(uriEncoded1);
+
+console.log(uriEncoded2);
+
+console.log(decodeURI(uriEncoded1));
